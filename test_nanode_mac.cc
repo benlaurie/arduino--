58f66fe399c26c99c2ea4b030bded0677aaa999b
Ben Laurie <ben@links.org>
2012-01-04 06:25:58 +0000

    Add a soak test.

--- test_nanode_mac.cc
+++ test_nanode_mac.cc
@@ -14,6 +14,19 @@ int main()
     Serial.write(mac.ok() ? 'G' : 'B');
     for (byte n = 0; n < 6; ++n)
         Serial.writeHex(mac[n]);
+    if (!mac.ok())
+        return 1;
+
+    for ( ; ; )
+        {
+        NanodeMAC mac2;
+
+        Serial.write(mac2.ok() ? 'G' : 'B');
+        for (byte n = 0; n < 6; ++n)
+            Serial.writeHex(mac2[n]);
+        if (!mac2.ok())
+            return 1;
+        }
 
     return 0;
     }
