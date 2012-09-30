6c587768ef7a7855151c7ec2bc99a78f4c5b008a
Ben Laurie <ben@links.org>
2012-09-30 09:33:20 +0100

    Add Ethernet support.

--- test/test_star_bridge.cc
+++ test/test_star_bridge.cc
@@ -1,6 +1,8 @@
 #include "arduino--.h"
+#include "ip.h"
 #include "rf12star.h"
 #include "serial.h"
+#include "tcp_server.h"
 
 class SerialObserver
     {
@@ -38,14 +40,61 @@ public:
 
 typedef StarMaster<RF12Star, SerialObserver> Master;
 
+typedef ENC28J60<Pin::B0> Ethernet;
+
+typedef IP<Ethernet> MyIP;
+
+static uint8_t mymac[6] = {0x54,0x55,0x58,0x10,0x00,0x24}; 
+static uint8_t myip[4] = {192,168,1,111};
+
+void setup()
+    {
+    /*initialize enc28j60*/
+    Ethernet::setup(mymac);
+
+    //init the ethernet/ip layer:
+    MyIP::init_ip_arp_udp_tcp(mymac, myip);
+    }
+
+class MyTCPServer : public TCPServer<MyIP, 80>
+    {
+    void packetReceived();
+    };
+
+void MyTCPServer::packetReceived()
+    {
+    clearBuffer();
+    char *buf = getData();
+    if (strncmp("GET / ", buf, 6) != 0)
+	// head, post and other methods for possible status codes see:
+	// http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+	add_p(PSTR("HTTP/1.0 501 Not OK\r\nContent-Type: text/html\r\n\r\n"));
+    else
+	add_p(PSTR("HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\nHi mum"));
+    }
+
+// FIXME: why do I need this?
+extern "C" void __cxa_pure_virtual()
+    {
+    Serial.write("Ooops!");
+    for( ; ; )
+	;
+    }
+
 int main()
     {
+    MyTCPServer tcp;
+
     Arduino::init();
     Serial.begin(57600);
     Master::init();
+    setup();
 
     Serial.write("Start\r\n");
 
     for ( ; ; )
+	{
 	Master::poll();
+	tcp.poll();
+	}
     }
